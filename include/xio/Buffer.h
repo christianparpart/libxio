6a6d8d3654e76e0a904b81eef4e84e47a71868f0
Christian Parpart <trapni@gmail.com>
2013-10-04 17:16:59 +0200

    Buffer: vprintf() added

--- include/xio/Buffer.h
+++ include/xio/Buffer.h
@@ -15,6 +15,7 @@
 #include <cassert>
 #include <string>
 #include <stdexcept>
+#include <cstdarg>
 
 namespace xio {
 
@@ -285,6 +286,8 @@ public:
 	void push_back(const std::string& value);
 	void push_back(const void *value, size_t size);
 	template<typename PodType, size_t N> void push_back(PodType (&value)[N]);
+
+	MutableBuffer& vprintf(const char* fmt, va_list args);
 
 	template<typename... Args>
 	MutableBuffer& printf(const char* fmt, Args... args);
@@ -1158,6 +1161,35 @@ inline void MutableBuffer<ensure>::push_back(const void* value, size_t size)
 	}
 }
 
+template<bool (*ensure)(void*, size_t)>
+inline MutableBuffer<ensure>& MutableBuffer<ensure>::vprintf(const char* fmt, va_list args)
+{
+	reserve(size() + strlen(fmt) + 1);
+
+	while (true) {
+		va_list va;
+		va_copy(va, args);
+		ssize_t buflen = vsnprintf(data_ + size_, capacity_ - size_, fmt, va);
+		va_end(va);
+
+		if (buflen >= -1 && buflen < static_cast<ssize_t>(capacity_ - size_)) {
+			resize(size_ + buflen);
+			break; // success
+		}
+
+		buflen = buflen > -1
+			? buflen + 1      // glibc >= 2.1
+			: capacity_ * 2;  // glibc <= 2.0
+
+		if (!setCapacity(capacity_ + buflen)) {
+			// increasing capacity failed
+			data_[capacity_ - 1] = '\0';
+			break; // alloc failure
+		}
+	}
+
+	return *this;
+}
 template<bool (*ensure)(void*, size_t)>
 template<typename... Args>
 inline MutableBuffer<ensure>& MutableBuffer<ensure>::printf(const char* fmt, Args... args)
